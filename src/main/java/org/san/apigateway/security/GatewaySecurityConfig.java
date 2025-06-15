c8ada40b84a92c6e6662d8252a5544dd1709941c
Supunsan <supun-san@users.noreply.github.com>
2025-06-15 19:52:31 +0530

    fix: allow unauthenticated access to /actuator/health for Consul health checks

--- src/main/java/org/san/apigateway/security/GatewaySecurityConfig.java
+++ src/main/java/org/san/apigateway/security/GatewaySecurityConfig.java
@@ -1,11 +1,13 @@
 package org.san.apigateway.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 @EnableWebFluxSecurity
 public class GatewaySecurityConfig {
 
